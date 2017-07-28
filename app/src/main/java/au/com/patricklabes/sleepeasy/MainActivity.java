ded1fa926ca115ecc89b42388ecd762eeca052c1
starfish01 <patrick.labes@gmail.com>
2017-07-29 07:11:14 +1000

    29/7
    basic notification added called from main activity
    has intent attached to navigate back to the app (mainactivity.class)

---
+++ app/src/main/java/au/com/patricklabes/sleepeasy/MainActivity.java
@@ -0,0 +1,58 @@
+package au.com.patricklabes.sleepeasy;
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.NotificationCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        notifyMe();
+
+    }
+
+
+
+    public void notifyMe(){
+
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(this);
+                mBuilder
+                .setSmallIcon(R.mipmap.ic_warning)
+                .setContentTitle("Battery Warning")
+                .setContentText("It appears you might not have your device plugged in");
+
+
+        Intent resultIntent = new Intent(this, MainActivity.class);
+        PendingIntent resultPendingIntent =
+                PendingIntent.getActivity(
+                        this,
+                        0,
+                        resultIntent,
+                        PendingIntent.FLAG_UPDATE_CURRENT
+                );
+
+        mBuilder.setContentIntent(resultPendingIntent);
+
+
+
+        int mBuilderID = 001;
+
+        NotificationManager mNotify =
+                (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        mNotify.notify(mBuilderID,mBuilder.build());
+
+
+
+    }
+
+
+
+}
