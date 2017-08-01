1385550c261294543d25f8c1662717080cb47dd0
starfish01 <patrick.labes@gmail.com>
2017-08-01 20:28:20 +1000

    1/8
    charge is displayed along with battery being connected or disconnected

--- app/src/main/java/au/com/patricklabes/sleepeasy/MainActivity.java
+++ app/src/main/java/au/com/patricklabes/sleepeasy/MainActivity.java
@@ -1,20 +1,16 @@
 package au.com.patricklabes.sleepeasy;
 
-import android.app.NotificationManager;
-import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.os.BatteryManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.app.NotificationCompat;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView textView;
+    private TextView textView, info2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,13 +18,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         textView = (TextView)this.findViewById(R.id.infoText);
+        info2 = (TextView)this.findViewById(R.id.info2);
 
 
         this.registerReceiver(this.mBatInfoReceiver,
                 new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
 
 
+        this.registerReceiver(this.chargeReceiver,
+                new IntentFilter(Intent.ACTION_POWER_CONNECTED));
 
+        this.registerReceiver(this.chargeReceiveroff,
+                new IntentFilter(Intent.ACTION_POWER_DISCONNECTED));
 
 
     }
@@ -41,8 +42,26 @@ public class MainActivity extends AppCompatActivity {
         public void onReceive(Context context, Intent intent) {
             int level = intent.getIntExtra("level",0);
             textView.setText(String.valueOf(level)+"%");
+
+
         }
     };
+
+    private BroadcastReceiver chargeReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+           info2.setText("power connected");
+        }
+    };
+
+    private BroadcastReceiver chargeReceiveroff = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            info2.setText("power disconnected");
+        }
+    };
+
+
 
 
 
