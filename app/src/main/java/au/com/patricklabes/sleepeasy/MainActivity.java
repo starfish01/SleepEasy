3abfa04af9d16dd3de7eb94fa72ac40fa7767478
starfish01 <patrick.labes@gmail.com>
2017-08-01 19:42:27 +1000

    1/8
    displays battery percentage

--- app/src/main/java/au/com/patricklabes/sleepeasy/MainActivity.java
+++ app/src/main/java/au/com/patricklabes/sleepeasy/MainActivity.java
@@ -3,7 +3,9 @@ package au.com.patricklabes.sleepeasy;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.BatteryManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -12,21 +14,36 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView textView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        textView = (TextView)this.findViewById(R.id.infoText);
 
-        Intent serviceIntent = new Intent(this, BatteryService.class);
-        startService(serviceIntent);
 
-        int isCharging = BatteryManager.BATTERY_STATUS_CHARGING;
+        this.registerReceiver(this.mBatInfoReceiver,
+                new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
 
-        TextView textView = (TextView) findViewById(R.id.infoText);
-        textView.setText(Integer.toString(isCharging));
+
+
+
 
     }
+
+
+
+
+    private BroadcastReceiver mBatInfoReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            int level = intent.getIntExtra("level",0);
+            textView.setText(String.valueOf(level)+"%");
+        }
+    };
+
 
 
 
