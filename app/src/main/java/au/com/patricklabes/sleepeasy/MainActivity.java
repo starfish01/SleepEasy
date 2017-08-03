eb2bbc18cb5de3275754c7675111ffa5419c10f7
starfish01 <patrick.labes@gmail.com>
2017-08-03 17:20:41 +1000

    3/8
    Not much still playing around how to read battery correctly
    left a note on where an example is

--- app/src/main/java/au/com/patricklabes/sleepeasy/MainActivity.java
+++ app/src/main/java/au/com/patricklabes/sleepeasy/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.os.BatteryManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
@@ -20,17 +21,27 @@ public class MainActivity extends AppCompatActivity {
         textView = (TextView)this.findViewById(R.id.infoText);
         info2 = (TextView)this.findViewById(R.id.info2);
 
-
+/*
         this.registerReceiver(this.mBatInfoReceiver,
                 new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
-
-/*
-        this.registerReceiver(this.chargeReceiver,
-                new IntentFilter(Intent.ACTION_POWER_CONNECTED));
-
-        this.registerReceiver(this.chargeReceiveroff,
-                new IntentFilter(Intent.ACTION_POWER_DISCONNECTED));
 */
+        IntentFilter ifilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+        Intent batteryStatus = getApplicationContext().registerReceiver(null, ifilter);
+
+        /*
+        https://stackoverflow.com/questions/3291655/get-battery-level-and-state-in-android
+         */
+
+        BatteryManager bm = (BatteryManager)getSystemService(BATTERY_SERVICE);
+        int batLevel = bm.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY);
+
+        int status = batteryStatus.getIntExtra("level", -1);
+        boolean isCharging = status == BatteryManager.BATTERY_STATUS_CHARGING ||
+                status == BatteryManager.BATTERY_STATUS_FULL;
+
+        textView.setText(String.valueOf(status));
+
+
 
     }
 
