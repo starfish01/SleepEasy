a218ea84cde9ef34b9f011fb4bbc175d02d60738
starfish01 <patrick.labes@gmail.com>
2017-08-21 20:19:33 +1000

    21/8

--- app/src/main/java/au/com/patricklabes/sleepeasy/MainActivity.java
+++ app/src/main/java/au/com/patricklabes/sleepeasy/MainActivity.java
@@ -3,7 +3,6 @@ package au.com.patricklabes.sleepeasy;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Switch;
