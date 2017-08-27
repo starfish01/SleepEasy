3e156cd4114c3453268a2767b5c5828c841b787e
starfish01 <patrick.labes@gmail.com>
2017-08-27 20:30:54 +1000

    27/8
    Started work on the notification

--- app/src/main/java/au/com/patricklabes/sleepeasy/Notification.java
+++ app/src/main/java/au/com/patricklabes/sleepeasy/Notification.java
@@ -1,8 +1,11 @@
 package au.com.patricklabes.sleepeasy;
 
+import android.app.Activity;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.v7.app.NotificationCompat;
 
 import static android.content.Context.NOTIFICATION_SERVICE;
@@ -11,11 +14,15 @@ import static android.content.Context.NOTIFICATION_SERVICE;
  * Created by patri on 31-Jul-17.
  */
 
-public class Notification {
+public class Notification extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
 
 
 
-
+    }
 
 
 
