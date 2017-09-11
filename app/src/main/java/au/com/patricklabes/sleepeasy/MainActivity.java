0dc2d142fec02f4f21ea1648117e0bd9feb0a098
starfish01 <patrick.labes@gmail.com>
2017-09-12 06:23:11 +1000

    12/9
    Set up ready to test alert function under realistic conditions

--- app/src/main/java/au/com/patricklabes/sleepeasy/MainActivity.java
+++ app/src/main/java/au/com/patricklabes/sleepeasy/MainActivity.java
@@ -79,9 +79,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
                  break;
              case R.id.btn_activate:
-                 //activeBtnChecker();
+                 activeBtnChecker();
 
-                 testExample();
+                 //testExample();
                  break;
 
          }
